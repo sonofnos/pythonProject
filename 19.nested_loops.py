a50bdd363948d2f450d03d9e333317ae99e26884
Christian C.C. Emeka <54124037+sonofnos@users.noreply.github.com>
2022-11-08 13:48:13 +0100

    nested_loops commit

---
+++ 19.nested_loops.py
@@ -0,0 +1,10 @@
+# for x in range(4):
+#     print(x)
+#
+# for x in range(4):
+#     for y in range(3):
+#         print(f'{x}, {y}')
+numbers = [5, 2, 5, 2, 2]
+
+for x in numbers:
+    print("x" * x)
