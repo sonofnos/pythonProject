321ec58d339e189f9c17c3a24986f1b926c26685
Christian C.C. Emeka <54124037+sonofnos@users.noreply.github.com>
2022-11-08 13:51:43 +0100

    nested_loops commit

--- 19.nested_loops.py
+++ 19.nested_loops.py
@@ -4,7 +4,13 @@
 # for x in range(4):
 #     for y in range(3):
 #         print(f'{x}, {y}')
-numbers = [5, 2, 5, 2, 2]
+# numbers = [5, 2, 5, 2, 2]
 
+numbers = [2, 2, 2, 6, 6]
 for x in numbers:
-    print("x" * x)
+    # print("x" * x)
+    output = ''
+    for count in range(x):
+        output += 'x'
+    print(output)
+
